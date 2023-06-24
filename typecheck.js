89f35fe6e08843210b7aa19326d68844c48e5fb0
Akash Goswami <116135533+Akashbuilds@users.noreply.github.com>
2023-06-24 06:54:57 +0530

    Add files via upload

---
+++ typecheck.js
@@ -0,0 +1,8 @@
+console.log(typeof NaN);
+console.log(typeof Infinity);
+console.log(typeof -0);
+console.log(typeof(Symbol));
+console.log(typeof undefined);
+console.log(typeof null);
+console.log(typeof (true && false));
+console.log(typeof this);
